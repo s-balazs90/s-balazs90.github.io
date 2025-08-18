48514b3a45d206f4a99ebf91d39b50cc198a8434
s-balazs90 <sarga.balazs@gmail.com>
2025-08-18 17:56:47 +0200

    js fájl módosítása

--- sarga-balazs-janos-front-end_vizsgamunka/js/main-java.js
+++ sarga-balazs-janos-front-end_vizsgamunka/js/main-java.js
@@ -38,10 +38,8 @@ window.addEventListener('click', (e) => {
 });
 
 // Scroll to top
-// Get the button:
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -52,10 +50,10 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
 }
 
 function topFunction() {
